6799258c1a029f7a8815781f30959f548778ab22
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-04-16 08:28:38 +0300

    Make corpus locally

--- chatterbot/corpus.py
+++ chatterbot/corpus.py
@@ -3,7 +3,7 @@ Seamlessly import the external chatterbot corpus module.
 View the corpus on GitHub at https://github.com/gunthercox/chatterbot-corpus
 """
 
-from chatterbot_corpus import Corpus
+from chatterbot_corpus.corpus import Corpus
 
 
 __all__ = (
